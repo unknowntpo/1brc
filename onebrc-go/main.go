d5cede9f902bdb9a848532578c54dc20fd746226
unknowntpo <e850506@gmail.com>
2024-03-11 19:16:21 +0800

    feat(1brc/onebrc_go): first attempt, single goroutine, bufio.Scanner

--- onebrc-go/main.go
+++ onebrc-go/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func must[T any](val T, err error) T {
@@ -13,12 +16,41 @@ func must[T any](val T, err error) T {
 	return val
 }
 
+type result struct {
+	city  string
+	min   float64
+	max   float64
+	mean  float64
+	acc   float64
+	count int
+}
+
+func compute(scanner *bufio.Scanner) (map[string]result, error) {
+	m := make(map[string]result, 100)
+	for scanner.Scan() {
+		vals := strings.Split(scanner.Text(), ";")
+		city := vals[0]
+		temp := must(strconv.ParseFloat(vals[1], 64))
+		if res, has := m[city]; has {
+			m[city] = result{city: city, min: temp, max: temp, acc: 1, count: 1}
+		} else {
+			res.count++
+			res.acc += temp
+			res.min = math.Min(res.min, temp)
+			res.max = math.Max(res.max, temp)
+			res.mean = res.acc / float64(res.count)
+			res.min = math.Min(res.min, temp)
+			m[city] = res
+		}
+	}
+	return m, nil
+}
+
 func main() {
 	filePath := "./data/weather_stations.csv"
 	f := must(os.Open(filePath))
 	defer f.Close()
 	scanner := bufio.NewScanner(bufio.NewReader(f))
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
+	m := must(compute(scanner))
+	fmt.Println(m)
 }
