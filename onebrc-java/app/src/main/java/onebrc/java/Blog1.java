55d39e6bb38e57ed086ce931751bba3c88dec9ff
unknowntpo <e850506@gmail.com>
2024-03-12 07:03:58 +0800

    feat(1brc/onebrc-java): add example from https://questdb.io/blog/billion-row-challenge-step-by-step/

---
+++ onebrc-java/app/src/main/java/onebrc/java/Blog1.java
@@ -0,0 +1,39 @@
+package onebrc.java;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+import static java.lang.Double.parseDouble;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summarizingDouble;
+
+public class Blog1 {
+    public static void main(String[] args) throws Exception {
+        var start = System.currentTimeMillis();
+        calculate();
+        System.err.format("Took %,d ms\n", System.currentTimeMillis() - start);
+    }
+
+    private static void calculate() throws Exception {
+        var allStats = new BufferedReader(new FileReader("../data/measurements.txt"))
+                .lines()
+                .parallel()
+                .collect(
+                        groupingBy(line -> line.substring(0, line.indexOf(';')),
+                                summarizingDouble(line ->
+                                        parseDouble(line.substring(line.indexOf(';') + 1)))));
+        var result = allStats.entrySet().stream().collect(Collectors.toMap(
+                Entry::getKey,
+                e -> {
+                    var stats = e.getValue();
+                    return String.format("%.1f/%.1f/%.1f",
+                            stats.getMin(), stats.getAverage(), stats.getMax());
+                },
+                (l, r) -> r,
+                TreeMap::new));
+        System.out.println(result);
+    }
+}
